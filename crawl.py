5960e12f29c26930615860200255173179e5b796
lijin <lj0014@gmail.com>
2013-04-07 14:26:18 +0800

    fix re and fix response encode

--- crawl.py
+++ crawl.py
@@ -5,26 +5,31 @@
 #http://list.jd.com/737-794-880-0-0-0-0-0-0-0-1-1-2-19-1607-33.html
 #http://list.jd.com/737-794-880-0-0-0-0-0-0-0-1-1-%d-19-1607-33.html
 
-
+import time
 import urllib2
 import re
 
 START_PAGE = 1
 MAX_PAGE = 6
 URL_FORMAT = 'http://list.jd.com/737-794-880-0-0-0-0-0-0-0-1-1-%d-19-1607-33.html'
-RE_FORMAT = re.compile(r'<a.*?href="(.*?)">已有(\d+?)人评价</a>')
+RE_FORMAT = re.compile('<span class=\'evaluate\'><a.*?href=\'(.*?)\'>已有(\d+?)人评价</a></span>')
 
 def main():
     result = []
     for i in range(START_PAGE,MAX_PAGE+1):
         url = URL_FORMAT % i
         try:
-            response = urllib2.urlopen(url,timeout=10)
+            request = urllib2.Request(url)
+            opener = urllib2.build_opener()
+            request.add_header('User-Agent','Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31')
+            response = opener.open(request)
         except Exception,e:
             print 'download error: '+str(e)
         response = response.read()
-        result += RE_FORMAT.findall(response)
-    sorted_result = sorted(result,key=lambda x:x[1],reverse=True) 
+        response = unicode(response,'gbk').encode('utf-8')
+        result += RE_FORMAT.findall(response,re.S)
+        time.sleep(1)
+    sorted_result = sorted(result,key=lambda x:int(x[1]),reverse=True) 
     print sorted_result[:10]
         
 if __name__ == '__main__':
