eba82b4cabe76086c2451cc339355dfb989e64ca
lijin <lj0014@gmail.com>
2013-04-07 14:43:12 +0800

    通过参数读取任意列表页

--- crawl.py
+++ crawl.py
@@ -8,13 +8,27 @@
 import time
 import urllib2
 import re
+import sys
 
 START_PAGE = 1
 MAX_PAGE = 6
 URL_FORMAT = 'http://list.jd.com/737-794-880-0-0-0-0-0-0-0-1-1-%d-19-1607-33.html'
 RE_FORMAT = re.compile('<span class=\'evaluate\'><a.*?href=\'(.*?)\'>已有(\d+?)人评价</a></span>')
 
+def read_input():
+    usage_str = 'Usage: python %s <list_url_format> <list_max_page>'
+    usage_str = usage_str % sys.argv[0]
+    if len(sys.argv) != 3:
+        print '!!!command error!!!'
+        print usage_str
+        sys.exit(-1)
+    else:
+        global URL_FORMAT,MAX_PAGE
+        URL_FORMAT = sys.argv[1]
+        MAX_PAGE = int(sys.argv[2])
+
 def main():
+    read_input()
     result = []
     for i in range(START_PAGE,MAX_PAGE+1):
         url = URL_FORMAT % i
